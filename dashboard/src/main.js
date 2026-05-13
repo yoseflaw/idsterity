f80e8ad930b1ba98776086de2cfdedd9ae10f228
Yosef Ardhito <yosefardhitowin@gmail.com>
2026-05-13 18:17:03 +0700

    feat(01-01): install scrollama + @fontsource packages, migrate fonts from CDN to self-hosted
    - npm install scrollama ^3.2.0, @fontsource/libre-baskerville, @fontsource/source-serif-4, @fontsource/jetbrains-mono
    - Prepend 5 @fontsource CSS imports to dashboard/src/main.js (Libre Baskerville 400/700/400-italic, Source Serif 4 400, JetBrains Mono 400)
    - Remove 3 Google Fonts CDN <link> tags from dashboard/index.html (FOUND-06)

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,3 +1,9 @@
+import '@fontsource/libre-baskerville/400.css';
+import '@fontsource/libre-baskerville/700.css';
+import '@fontsource/libre-baskerville/400-italic.css';
+import '@fontsource/source-serif-4/400.css';
+import '@fontsource/jetbrains-mono/400.css';
+
 import { mount } from 'svelte'
 import App from './App.svelte'
 
